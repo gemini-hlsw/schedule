e38234dbcdb1670688a3e1f8ce7268be3d3e6f5a
Sergio <sergio.troncoso@noirlab.edu>
2023-02-14 16:09:09 -0300

    SCHED-297: Working GraphQL connection

--- src/gql/graphql.ts
+++ src/gql/graphql.ts
@@ -89,3 +89,84 @@ export type GetPlansBySiteQuery = { __typename?: 'Query', sitePlans: Array<{ __t
 
 
 export const GetPlansBySiteDocument = {"kind":"Document","definitions":[{"kind":"OperationDefinition","operation":"query","name":{"kind":"Name","value":"GetPlansBySite"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"sitePlans"},"arguments":[{"kind":"Argument","name":{"kind":"Name","value":"site"},"value":{"kind":"EnumValue","value":"GS"}}],"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"nightIdx"}},{"kind":"Field","name":{"kind":"Name","value":"plansPerSite"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"site"}},{"kind":"Field","name":{"kind":"Name","value":"startTime"}},{"kind":"Field","name":{"kind":"Name","value":"endTime"}},{"kind":"Field","name":{"kind":"Name","value":"visits"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"startTime"}},{"kind":"Field","name":{"kind":"Name","value":"obsId"}},{"kind":"Field","name":{"kind":"Name","value":"atomStartIdx"}},{"kind":"Field","name":{"kind":"Name","value":"atomEndIdx"}}]}}]}}]}}]}}]} as unknown as DocumentNode<GetPlansBySiteQuery, GetPlansBySiteQueryVariables>;
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: string;
+  String: string;
+  Boolean: boolean;
+  Int: number;
+  Float: number;
+  /** Date with time (isoformat) */
+  DateTime: any;
+};
+
+export type CreateNewScheduleInput = {
+  endTime: Scalars['String'];
+  startTime: Scalars['String'];
+};
+
+export type Mutation = {
+  __typename?: 'Mutation';
+  newSchedule: NewScheduleResponse;
+};
+
+
+export type MutationNewScheduleArgs = {
+  newScheduleInput: CreateNewScheduleInput;
+};
+
+export type NewScheduleError = {
+  __typename?: 'NewScheduleError';
+  error: Scalars['String'];
+};
+
+export type NewScheduleResponse = NewScheduleError | NewScheduleSuccess;
+
+export type NewScheduleSuccess = {
+  __typename?: 'NewScheduleSuccess';
+  success: Scalars['Boolean'];
+};
+
+export type Query = {
+  __typename?: 'Query';
+  allPlans: Array<SPlans>;
+  plans: Array<SPlans>;
+  sitePlans: Array<SPlans>;
+};
+
+
+export type QuerySitePlansArgs = {
+  site: Site;
+};
+
+export type SPlan = {
+  __typename?: 'SPlan';
+  endTime: Scalars['DateTime'];
+  site: Site;
+  startTime: Scalars['DateTime'];
+  visits: Array<SVisit>;
+};
+
+export type SPlans = {
+  __typename?: 'SPlans';
+  nightIdx: Scalars['Int'];
+  plansPerSite: Array<SPlan>;
+};
+
+export type SVisit = {
+  __typename?: 'SVisit';
+  atomEndIdx: Scalars['Int'];
+  atomStartIdx: Scalars['Int'];
+  obsId: Scalars['String'];
+  startTime: Scalars['DateTime'];
+};
+
+export enum Site {
+  Gn = 'GN',
+  Gs = 'GS'
+}
+
+export type GetPlansBySiteQueryVariables = Exact<{ [key: string]: never; }>;
+
+
+export type GetPlansBySiteQuery = { __typename?: 'Query', sitePlans: Array<{ __typename?: 'SPlans', nightIdx: number, plansPerSite: Array<{ __typename?: 'SPlan', site: Site, startTime: any, endTime: any, visits: Array<{ __typename?: 'SVisit', startTime: any, obsId: string, atomStartIdx: number, atomEndIdx: number }> }> }> };
