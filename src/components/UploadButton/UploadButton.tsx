e0c5a8620c35a041834de6b0aa7d782fb97bc110
Diego Gomez <diego.gomez@noirlab.edu>
2023-12-27 11:25:34 -0300

    Fixed Upload button style

--- src/components/UploadButton/UploadButton.tsx
+++ src/components/UploadButton/UploadButton.tsx
@@ -1,68 +1,77 @@
-import React, { useRef } from 'react'
-import { FileUpload } from 'primereact/fileupload'
-import { ProgressBar } from 'primereact/progressbar'
-import { Toast } from 'primereact/toast'
-import { Tooltip } from 'primereact/tooltip'
+import { useRef } from "react";
+import { FileUpload } from "primereact/fileupload";
+import { Toast } from "primereact/toast";
+import { Tooltip } from "primereact/tooltip";
 
-import './UploadButton.scss'
-
+import "./UploadButton.scss";
 
 interface UploadProps {
-  label: string
+  label: string;
 }
 
 export default function UploadButton({ label }: UploadProps) {
-  const toast = useRef<Toast>(null)
+  const toast = useRef<Toast>(null);
 
   const onUpload = () => {
-    toast.current?.show({ severity: 'info', summary: 'Success', detail: 'File Uploaded' })
-  }
+    toast.current?.show({
+      severity: "info",
+      summary: "Success",
+      detail: "File Uploaded",
+    });
+  };
 
   const headerTemplate = (options: any) => {
     const { className, chooseButton, uploadButton, cancelButton } = options;
 
     return (
-      <div className={className} style={{ backgroundColor: 'transparent', display: 'flex', alignItems: 'center' }}>
-        <div className="flex align-items-right gap-3 ml-auto" style={{ marginLeft: 1 }}>
+      <div
+        className={className}
+        style={{
+          backgroundColor: "transparent",
+          display: "flex",
+          alignItems: "center",
+        }}
+      >
+        <div
+          className="flex align-items-right gap-3 ml-auto"
+          style={{ marginLeft: 1 }}
+        >
           <span>{label}</span>
         </div>
         {chooseButton}
         {uploadButton}
         {cancelButton}
       </div>
-    )
-  }
+    );
+  };
 
   const itemTemplate = (file: any, props: any) => {
     return (
       <div className="flex align-items-left flex-wrap">
-        <div className="flex align-items-left" style={{ width: '20%' }}>
-          <span className="flex flex-column text-left ml-3">
-            {file.name}
-          </span>
+        <div className="flex align-items-left" style={{ width: "20%" }}>
+          <span className="flex flex-column text-left ml-3">{file.name}</span>
         </div>
       </div>
-    )
-  }
-
+    );
+  };
 
   const chooseOptions = {
-    icon: 'pi pi-fw pi-images',
+    icon: "pi pi-fw pi-images",
     iconOnly: true,
-    className: 'custom-choose-btn p-button-rounded p-button-outlined'
-  }
+    className: "custom-choose-btn p-button-rounded",
+  };
 
   const uploadOptions = {
-    icon: 'pi pi-fw pi-cloud-upload',
+    icon: "pi pi-fw pi-cloud-upload",
     iconOnly: true,
-    className: 'custom-upload-btn p-button-success p-button-rounded p-button-outlined'
-  }
+    className: "custom-upload-btn p-button-success p-button-rounded",
+  };
 
   const cancelOptions = {
-    icon: 'pi pi-fw pi-times',
+    icon: "pi pi-fw pi-times",
     iconOnly: true,
-    className: 'custom-cancel-btn p-button-danger p-button-rounded p-button-outlined'
-  }
+    className: "custom-cancel-btn p-button-danger p-button-rounded",
+  };
 
   return (
     <>
@@ -73,7 +82,7 @@ export default function UploadButton({ label }: UploadProps) {
       <FileUpload
         className="smaller-upload"
         name="demo[]"
-        url={'/api/upload'}
+        url={"/api/upload"}
         onUpload={onUpload}
         accept="image/*"
         maxFileSize={1000000}
