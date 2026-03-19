dc2f056844c75b5cd1b6d6e9fefd257f6340c205
Diego Gomez <diego.gomez@noirlab.edu>
2026-03-19 16:03:02 -0300

    Update program list using refLabel instead of id on display

--- src/components/BuildParameters/query.ts
+++ src/components/BuildParameters/query.ts
@@ -2,6 +2,9 @@ import { graphql } from "@/gql";
 
 export const getProgramList = graphql(`
   query availablePrograms {
-    availablePrograms
+    availablePrograms {
+      id
+      refLabel
+    }
   }
 `);
